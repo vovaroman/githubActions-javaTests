bb6b228dc5ec0f0f11c0267228c54417df44d212
Vova Roman <vovan.roman7@gmail.com>
2025-08-20 10:25:42 -0400

    Create Calculator.java

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Calculator {
+
+    /** Adds two integers. */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /** Subtracts b from a. */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
